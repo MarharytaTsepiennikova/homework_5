cbc8d5874a645f0088c0a626cd548c925746fbbb
rita <m.tsepiennikova@mail.com>
2018-04-13 11:16:40 +0300

    edited

--- src/Homework5_4.java
+++ src/Homework5_4.java
@@ -8,10 +8,13 @@ public class Homework5_4 {
     public static void main(String[] args) {
         int[] array1 = {3, 1, 6, -2, 8};
         int[] array2 = {-1, 4, 3, -3, 12};
-        System.out.println(makeArr(array1, array2));
+        String newArr = makeArr(array1, array2);
+        System.out.println(newArr);
     }
     static String makeArr(int[] array1, int[] array2){
-        int[] result = new int[10];
+        int elements = 0;
+        elements = array1.length + array2.length;
+        int[] result = new int[elements];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, 5, array2.length);
         String strResult = Arrays.toString(result);
