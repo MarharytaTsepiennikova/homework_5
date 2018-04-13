b306f21881273947e50069abcdeb013cbdf0184e
rita <m.tsepiennikova@mail.com>
2018-04-13 11:05:21 +0300

    edited

--- src/Homework5_2.java
+++ src/Homework5_2.java
@@ -9,17 +9,17 @@ import java.util.Arrays;
 public class Homework5_2 {
     public static void main(String[] args) {
         int[] array = {2, 1, 5, 6, 1, 9};
-        System.out.println(sumOfThreeElements(array));
+        int sum = sumOfThreeElements(array);
+        System.out.println(sum);
+        String strArray = Arrays.toString(array);
+        System.out.println(strArray);
 
     }
     static int sumOfThreeElements(int[] array){
         int result = 0;
         for (int index = 0; index < 3; index++) {
-            array[index] += 1;
+            array[index]++;
         }
-
-        String strArray = Arrays.toString(array);
-        System.out.println(strArray);
 
         for (int index = 0; index < 3; index++) {
             result += array[index];
